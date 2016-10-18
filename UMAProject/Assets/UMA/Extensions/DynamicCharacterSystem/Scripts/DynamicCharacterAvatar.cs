dc8d912bd8370fc67241fc8e370dacb636066856
Jaimi <jaimi@aztica.com>
2016-10-18 17:48:03 -0500

    Remove "button" from color button prefab.
    Also added gray background to text to improve visiblity

--- UMAProject/Assets/UMA/Extensions/DynamicCharacterSystem/Scripts/DynamicCharacterAvatar.cs
+++ UMAProject/Assets/UMA/Extensions/DynamicCharacterSystem/Scripts/DynamicCharacterAvatar.cs
@@ -1791,41 +1791,13 @@ namespace UMACharacterSystem
 			}
 		}
 
-       /* public class DnaSetter
-        {
-            public string Name; // The name of the DNA.
-            public float Value; // Current value of the DNA.
-
-            protected int OwnerIndex;    // position of DNA in index, created at initialization
-            protected UMADnaBase Owner;  // owning DNA class. Used to set the DNA by index
-
-            /// <summary>
-            /// Construct a DnaSetter
-            /// </summary>
-            /// <param name="name"></param>
-            /// <param name="value"></param>
-            /// <param name="ownerIndex"></param>
-            /// <param name="owner"></param>
-            public DnaSetter(string name, float value, int ownerIndex, UMADnaBase owner)
-            {
-                Name = name;
-                Value = value;
-                OwnerIndex = ownerIndex;
-                Owner = owner;
-            }
-
-            /// <summary>
-            /// Set the current DNA value. You will need to rebuild the character to see 
-            /// the results change.
-            /// </summary>
-            public void Set()
-            {
-                Owner.SetValue(OwnerIndex, Value);
-            }
-        } */
 		#endregion
 	}
 
+    /// <summary>
+    /// A DnaSetter is used to set a specific piece of DNA on the avatar
+    /// that it is pulled from.
+    /// </summary>
     public class DnaSetter
     {
         public string Name; // The name of the DNA.
@@ -1868,6 +1840,4 @@ namespace UMACharacterSystem
             Owner.SetValue(OwnerIndex, Value);
         }
     }
-
-
 }
